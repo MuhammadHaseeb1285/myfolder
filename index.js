f8dbf09f2476a1dd2f711596e172dda4621cc3a5
MuhammadHaseeb1285 <haseebarif1285@gmail.com>
2024-09-09 09:09:46 -0700

    these are the new fiels

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log(`hello`);
+console.log (`my name is haseeb`);
+
+window.alert(`hy bro`)
+
+let MYname="Muhammad Haseeb";
+let age=6;
+console.log(`my name uis ${MYname}`);
+console.log(`my age is ${age}`);
+
+document.getElementById("H1").textContent="this is the first heading";
+document.getAnimations("P1").textContent="This the paragraph of the context";
